11dd80d06de58c841638a3945e69021779273310
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-04-28 12:05:55 +0200

    fix: Remove `Dummy` struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,28 +4,20 @@ use std::{
     time::Duration,
 };
 
-pub static DUMMY: Dummy = Dummy::new();
-
-pub struct Dummy(Mutex<OnceLock<JoinHandle<()>>>);
-
-impl Dummy {
-    const fn new() -> Self {
-        Self(Mutex::new(OnceLock::new()))
-    }
-}
+pub static HANDLE: Mutex<OnceLock<JoinHandle<()>>> = Mutex::new(OnceLock::new());
 
 #[no_mangle]
 pub extern "C" fn init(timeout: libc::c_int) {
     let handle = thread::spawn(move || {
         thread::sleep(Duration::from_millis(timeout as u64));
     });
-    DUMMY.0.lock().unwrap().set(handle).unwrap();
+    HANDLE.lock().unwrap().set(handle).unwrap();
 
     unsafe { libc::atexit(handler) };
 }
 
 extern "C" fn handler() {
-    let handle = DUMMY.0.lock().unwrap().take().unwrap();
+    let handle = HANDLE.lock().unwrap().take().unwrap();
     handle.join().unwrap();
     eprintln!("Successfuly joined the thread!");
 }
