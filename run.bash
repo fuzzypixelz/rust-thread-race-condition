18aae3d8c78306086768999fb8b97444b5127cd5
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-04-28 11:33:30 +0200

    chore: List target/release contents

--- run.bash
+++ run.bash
@@ -5,6 +5,7 @@ set -xeo pipefail
 readonly basename=rust_thread_race_condition
 
 cargo build --release
+ls -hal target/release
 cp target/release/"$basename".dll.lib .
 cp target/release/"$basename".dll .
 
